5175cb3f0af719eb32793d230a3c353b4b9d1d04
Sina Kheiry <sinakheiri.dev@gmail.com>
2025-10-16 15:09:46 +0330

    feat: added t

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,12 @@
+import { useTranslations } from 'next-intl'
+
 export default function Home() {
+  const t = useTranslations('home')
+
   return (
     <div className="flex w-full flex-1 items-center justify-center">
       <div className="bg-card ring-primary text-muted-foreground rounded-md p-6 font-mono ring-2">
-        Hello World! <span className="animate-caret-blink text-primary pb-0.5">|</span>
+        {t('hello')} <span className="animate-caret-blink text-primary pb-0.5">|</span>
       </div>
     </div>
   )
